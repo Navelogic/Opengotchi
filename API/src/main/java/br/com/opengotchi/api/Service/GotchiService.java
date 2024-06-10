f02066b09a11a6bf316f37bf90ad6d3e4c7c6f58
Arian Weslley <weslleysantoszbd@gmail.com>
2024-06-10 15:08:26 -0300

    Adição de novos métodos.

--- API/src/main/java/br/com/opengotchi/api/Service/GotchiService.java
+++ API/src/main/java/br/com/opengotchi/api/Service/GotchiService.java
@@ -2,11 +2,18 @@ package br.com.opengotchi.api.Service;
 
 import br.com.opengotchi.api.Entitie.Gotchi;
 import br.com.opengotchi.api.Repository.GotchiRepository;
+import br.com.opengotchi.api.Util.API_URLs;
 import br.com.opengotchi.api.Util.ConsumoAPI;
+import br.com.opengotchi.api.Util.Model.DadosDigimon;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Service
 public class GotchiService {
@@ -25,9 +32,44 @@ public class GotchiService {
         gotchiRepository.deleteAll();
     }
 
-    // API
-    public String apiTest(){
+    // API - Digimon
+    public String apiTestDigimon(){
         var consumoAPI = new ConsumoAPI();
-        return consumoAPI.obterDadosAPI("https://jsonplaceholder.typicode.com/posts");
+        return consumoAPI.obterDadosAPI(API_URLs.DIGIMON + "1");
+    }
+
+    public String findDigimonById(String id){
+        var consumoAPI = new ConsumoAPI();
+        return consumoAPI.obterDadosAPI(API_URLs.DIGIMON + id);
+    }
+
+    public String findDigimonByName(String nome){
+        var consumoAPI = new ConsumoAPI();
+        return consumoAPI.obterDadosAPI(API_URLs.DIGIMON + nome);
+    }
+
+    public List<DadosDigimon> findAllDigimon() {
+        var consumoAPI = new ConsumoAPI();
+        var objectMapper = new ObjectMapper();
+        return IntStream.rangeClosed(1, 1460)
+                .parallel()
+                .mapToObj(i -> {
+                    String jsonResponse = consumoAPI.obterDadosAPI(API_URLs.DIGIMON + i);
+                    try {
+                        return objectMapper.readValue(jsonResponse, DadosDigimon.class);
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                })
+                .filter(Objects::nonNull)
+                .collect(Collectors.toList());
+    }
+
+
+    // API - Pokemon
+    public String apiTestPokemon(){
+        var consumoAPI = new ConsumoAPI();
+        return consumoAPI.obterDadosAPI(API_URLs.POKEMON + "1");
     }
 }
