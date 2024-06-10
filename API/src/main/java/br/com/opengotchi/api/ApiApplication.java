58f637c662ee525ff9d2042fa877a4d182240730
Arian Weslley <weslleysantoszbd@gmail.com>
2024-06-09 21:25:56 -0300

    Implementação do CommandLineRunner

--- API/src/main/java/br/com/opengotchi/api/ApiApplication.java
+++ API/src/main/java/br/com/opengotchi/api/ApiApplication.java
@@ -1,13 +1,18 @@
 package br.com.opengotchi.api;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class ApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("CommandLineRunner running...");
+	}
 }
