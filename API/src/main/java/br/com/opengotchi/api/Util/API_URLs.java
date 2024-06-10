19eb98f4c4317ad808fe7c8c102af4cacc0ba380
Arian Weslley <weslleysantoszbd@gmail.com>
2024-06-10 15:04:06 -0300

    Adição de classe para encapsular todas as bases de endpoints externos da aplicação.

---
+++ API/src/main/java/br/com/opengotchi/api/Util/API_URLs.java
@@ -0,0 +1,6 @@
+package br.com.opengotchi.api.Util;
+
+public class API_URLs {
+    public static final String POKEMON = "https://pokeapi.co/api/v2/pokemon/";
+    public static final String DIGIMON = "https://digi-api.com/api/v1/digimon/";
+}
