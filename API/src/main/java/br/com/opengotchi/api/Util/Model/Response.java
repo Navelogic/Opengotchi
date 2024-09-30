403013c03eb86d3bad28f58aa9c4adbbb34bcb5c
Arian Weslley <weslleysantoszbd@gmail.com>
2024-09-30 13:32:26 -0300

    Atualização de Utils

--- API/src/main/java/br/com/opengotchi/api/Util/Model/Response.java
+++ API/src/main/java/br/com/opengotchi/api/Util/Model/Response.java
@@ -1,12 +1,8 @@
 package br.com.opengotchi.api.Util.Model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Response {
